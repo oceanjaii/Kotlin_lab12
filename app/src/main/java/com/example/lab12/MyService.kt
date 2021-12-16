a5bfd81a19c6925e174db1e10f876a217184835b
oceanjaii <windfung16@gmail.com>
2021-12-16 20:23:21 +0800

    first

---
+++ app/src/main/java/com/example/lab12/MyService.kt
@@ -0,0 +1,45 @@
+package com.example.lab12
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+
+class MyService : Service() {
+
+    override fun onCreate(){
+        super.onCreate()
+
+        /*Thread {
+            try {
+                Thread.sleep(3000)
+                val intent = Intent(this,SecActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }.start() */
+
+        GlobalScope.launch(Dispatchers.Main) {
+           delay(3000)
+            val intent = Intent(this@MyService,SecActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+    }
+
+
+
+
+    override fun onStartCommand(intent: Intent, flags:Int, startid: Int): Int {
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? = null
+}
